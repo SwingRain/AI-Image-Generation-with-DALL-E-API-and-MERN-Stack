f5b2f1260104fdd4d720301a625645eda98735c6
Junwen <56602704+SwingRain@users.noreply.github.com>
2025-02-09 01:04:23 -0500

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -81,7 +81,7 @@ const   CreatePost = () => {
               labelName= "Your Name"
               type = "text"
               name = "name"
-              placeholder="Anuraj"
+              placeholder="Junwen"
               value={form.name}
               handleChange={handleChange}
             />
